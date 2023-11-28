bb1af657d04f7730a7bb605f1beeab0ee253c237
G3R74W <83832635+G3R74W@users.noreply.github.com>
2023-11-28 17:15:53 +0100

    started working on the project

---
+++ prog_mobile_ios/projet_prog/projet_prog/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  projet_prog
+//
+//  Created by Tobias Wendl on 28/11/2023.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath) as! TableViewCell
+        let row = indexPath.row
+        cell.legend.text = data[row].legend
+        cell.picture.image = UIImage(named: String(data[row].picture))
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        for i in 0...50 {
+            var legend = "legende de l'image " + String(i)
+            var picture = Int.random(in:1...10)
+            var d = MyData(legend: legend, picture : picture)
+            data.append(d)
+        }
+        
+        dataTableView.dataSource = self
+    }
+
+
+
+    @IBOutlet weak var dataTableView: UITableView!
+    var data = [MyData]()
+}
+
+
