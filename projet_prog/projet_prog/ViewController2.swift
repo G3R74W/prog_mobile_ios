518aa19d8e3951cb6e7ce704549730cbee5fde77
tobias <h,jhtrdgnh,fxgndhsrwdhgh,dgf>
2023-12-11 14:35:39 +0100

    increment et decrement des scores

--- projet_prog/projet_prog/ViewController2.swift
+++ projet_prog/projet_prog/ViewController2.swift
@@ -8,14 +8,39 @@
 import UIKit
 
 class ViewController2: UIViewController, UITextFieldDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         txt1.delegate = self
         txt2.delegate = self
+        
+        stepper1.minimumValue = 0
+        stepper1.maximumValue = 200
+        stepper1.stepValue = 1
+        stepper1.value = 0
+        
+        stepper2.minimumValue = 0
+        stepper2.maximumValue = 200
+        stepper2.stepValue = 1
+        stepper2.value = 0
+        
+        stepper1.addTarget(self, action: #selector(stepper1ValueChanged), for: .valueChanged)
+        
+        stepper2.addTarget(self, action: #selector(stepper2ValueChanged), for: .valueChanged)
+        
+        score1.text = "\(Int(stepper1.value))"
+        score2.text = "\(Int(stepper2.value))"
+    }
+    
+    @objc func stepper1ValueChanged(){
+        score1.text = "\(Int(stepper1.value))"
+    }
+    
+    @objc func stepper2ValueChanged(){
+        score2.text = "\(Int(stepper2.value))"
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -36,6 +61,8 @@ class ViewController2: UIViewController, UITextFieldDelegate {
         }
     }
     
+    //maj des equipes
+    
     @IBOutlet weak var team1: UILabel!
     
     @IBOutlet weak var team2: UILabel!
@@ -45,15 +72,16 @@ class ViewController2: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var txt2: UITextField!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    //augmentation des scores
+    @IBOutlet weak var score1: UILabel!
+    
+    @IBOutlet weak var score2: UILabel!
+    
+    
+    @IBOutlet weak var stepper1: UIStepper!
+    
+    @IBOutlet weak var stepper2: UIStepper!
+    
 }
+     
